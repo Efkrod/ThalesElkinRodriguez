c6aa6eb78697605fb90c306a7e72bf49d60ed3ea
Efkrod <eferneyrod@gmail.com>
2024-03-31 15:50:08 -0500

    Adición de documentación

--- ThalesElkinRodriguez/DAL/EmployeeService.cs
+++ ThalesElkinRodriguez/DAL/EmployeeService.cs
@@ -10,8 +10,11 @@ namespace ThalesElkinRodriguez.DAL
         public EmployeeService(HttpClient httpClient)
         {
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-        } 
-
+        }
+        /// <summary>
+        /// Obtiene la lista de empleados desde una API REST
+        /// </summary>
+        /// <returns>Una lista de objetos de tipo Employee que representan a los empleados.</returns>
         public async Task<List<Employee>> GetEmployees()
         {
             var response = await _httpClient.GetAsync("https://dummy.restapiexample.com/api/v1/employees");
@@ -24,7 +27,11 @@ namespace ThalesElkinRodriguez.DAL
             }
             return result.Data;
         }
-
+        /// <summary>
+        /// Obtiene la lista de empleados desde una API REST por identificador único
+        /// </summary>
+        /// <param name="id">El identificador único del empleado</param>
+        /// <returns>El objeto de tipo Employee que representa al empleado encontrado</returns>
         public async Task<Employee> GetEmployeeById (int id)
         {
             var response = await _httpClient.GetAsync($"https://dummy.restapiexample.com/api/v1/employee/{id}");
@@ -37,6 +44,10 @@ namespace ThalesElkinRodriguez.DAL
             }
             return result.Data;
         }
+        /// <summary>
+        /// Calcula los salarios anuales de todos los empleados.
+        /// </summary>
+        /// <returns>Una lista que representa los salarios anuales de los empleados.</returns>
         public async Task<List<int>> CalculateAnnualSalaries()
         {
             var employees = await GetEmployees();
@@ -49,7 +60,11 @@ namespace ThalesElkinRodriguez.DAL
 
             return annualSalaries;
         }
-
+        /// <summary>
+        /// Calcula el salario anual de un empleado específico por su identificador único.
+        /// </summary>
+        /// <param name="id">El identificador único del empleado.</param>
+        /// <returns>El salario anual del empleado.</returns>
         public async Task<int> CalculateAnnualSalaryById(int id)
         {
             var employee = await GetEmployeeById(id);
